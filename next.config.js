c9c8e8391da38c873c8cad5d9d399cf2c746cd27
dainnovation722 <722.dai.innovation@gmail.com>
2023-06-10 23:18:59 +0900

    wip

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.BASE_PATH ? "/" + process.env.BASE_PATH : "";
 
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: basePath,
-  basePath: basePath,
+  assetPrefix: "aarambh-residency",
+  basePath: "aarambh-residency",
 };
 
 module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-"use client";
 import type { NextPage } from "next";
 import { Hero } from "../components/hero";
 import { Box } from "../components/styles/box";
