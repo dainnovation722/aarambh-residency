fbe56e34b3b542f4ae97665bd1c81e8f5bab814f
dainnovation722 <722.dai.innovation@gmail.com>
2023-06-10 21:57:20 +0900

    :bug: assetPrefix追加

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.BASE_PATH ? "/" + process.env.BASE_PATH : "";
+
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   swcMinify: true,
-}
+  assetPrefix: basePath,
+  basePath: basePath,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
